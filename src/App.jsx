d1ff30d48a6f68c6cb11eef9bc083dea0dbebade
vikas-11-dixena <vikasdixena79@gmail.com>
2024-08-14 13:42:57 +0530

    faker data fetch

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import Header from './components/Header'
 import { BrowserRouter } from 'react-router-dom'
 import "./App.css"
+import Home from './components/Home'
+import Cart from './components/Cart'
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
+      <div className="App">
+        <Route path = "/" Component = {Home}/>
+        <Route path = "/cart" Component = {Cart}/>
+      </div>
     </BrowserRouter>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import faker from 'faker'
 
 const Home = () => {
+    const productArray = [...Array(20)].map(() => ({
+        id:faker.datatype.uuid(),
+        name:faker.commerce.productName(),
+        price:faker.commerce.Price(),
+        image:faker.random.image()
+    }));
+    
+    const [products] = useState(productArray)
+
   return (
-    <div>
-      Home
+    <div className='productContainer'>
+      {products.map((prod => (
+        <span>{prod.name}</span>
+      )))}
     </div>
   )
 }
